c08c47caa1bf213344f30337803f24bf9db59849
bogdanftry <117528354+bogdanftry@users.noreply.github.com>
2022-11-05 17:07:48 +0200

    Create speed.lua

---
+++ speed.lua
@@ -0,0 +1,45 @@
+
+game:GetService("Workspace").Living["plizen_go2"].RemoteFunction:InvokeServer("LearnSkill", {
+	["Skill"] = "Agility I", 
+	["SkillTreeType"] = "Character"
+})
+wait(1)
+
+game:GetService("Workspace").Living["plizen_go2"].RemoteFunction:InvokeServer("LearnSkill", {
+	["Skill"] = "Agility II", 
+	["SkillTreeType"] = "Character"
+})
+wait(2)
+
+game:GetService("Workspace").Living["plizen_go2"].RemoteFunction:InvokeServer("LearnSkill", {
+	["Skill"] = "Agility III", 
+	["SkillTreeType"] = "Character"
+})
+game:GetService("Workspace").Living["plizen_go2"].RemoteFunction:InvokeServer("LearnSkill", {
+	["Skill"] = "Worthiness I", 
+	["SkillTreeType"] = "Character"
+})
+wait(1)
+
+game:GetService("Workspace").Living["plizen_go2"].RemoteFunction:InvokeServer("LearnSkill", {
+	["Skill"] = "Worthiness II", 
+	["SkillTreeType"] = "Character"
+})
+wait(2)
+
+game:GetService("Workspace").Living["plizen_go2"].RemoteFunction:InvokeServer("LearnSkill", {
+	["Skill"] = "Worthiness III", 
+	["SkillTreeType"] = "Character"
+})
+wait(1)
+
+game:GetService("Workspace").Living["plizen_go2"].RemoteFunction:InvokeServer("LearnSkill", {
+	["Skill"] = "Worthiness IV", 
+	["SkillTreeType"] = "Character"
+})
+wait(2)
+
+game:GetService("Workspace").Living["plizen_go2"].RemoteFunction:InvokeServer("LearnSkill", {
+	["Skill"] = "Worthiness V", 
+	["SkillTreeType"] = "Character"
+})
